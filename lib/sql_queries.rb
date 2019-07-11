ca7b2232f661b2fbcccdf8371cdf80695aeb904a
Trish Ogen <patriciaogen@gmail.com>
2019-07-11 23:49:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,10 +7,11 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"select pr.title, pl.amount
+"select pr.title, sum(pl.amount)
 from projects pr
 join pledges pl
 on pr.id = pl.project_id
+group by pr.title
 order by pr.title;"
 end
 
