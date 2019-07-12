28ab2bc2a1f18874559a436576b806847b963ece
Trish Ogen <patriciaogen@gmail.com>
2019-07-12 00:00:07 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,9 +43,9 @@ order by total_pledged, u.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"select pl.category, pl.amount
-from pledges pl
-where pl.category = 'music';"
+"select category, amount
+from pledges
+where ategory = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
